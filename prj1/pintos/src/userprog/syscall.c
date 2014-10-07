622fc9b62ea9f470abda881e32c65805e4585c71
WofloW <lancefangw@gmail.com>
2014-10-07 15:14:26 -0700

    Restore changes in userprog

--- prj1/pintos/src/userprog/syscall.c
+++ prj1/pintos/src/userprog/syscall.c
@@ -3,7 +3,6 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
-#include "devices/shutdown.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -14,77 +13,8 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f) 
+syscall_handler (struct intr_frame *f UNUSED) 
 {
-  switch(*(f->esp))
-  {
-    /* Halt the operating system. */
-    case SYS_HALT:                  
-      shutdown_power_off();
-      break;
-    
-    /* Terminate this process. */
-    case SYS_EXIT:  
-      break;  
-    
-    /* Start another process. */
-    case SYS_EXEC:                  
-
-      break;
-    
-    /* Wait for a child process to die. */
-    case SYS_WAIT:                 
-
-      break;
-    
-    /* Create a file. */
-    case SYS_CREATE:                
-
-      break;
-    
-    /* Delete a file. */
-    case SYS_REMOVE:           
-
-      break;     
-    
-    /* Open a file. */
-    case SYS_OPEN:          
-
-      break;       
-    
-    /* Obtain a file's size. */
-    case SYS_FILESIZE:          
-
-      break;     
-    
-    /* Read from a file. */
-    case SYS_READ:            
-
-      break;    
-    
-    /* Write to a file. */  
-    case SYS_WRITE:            
-
-      break;     
-    
-    /* Change position in a file. */
-    case SYS_SEEK:
-
-      break;
-    
-    /* Report current position in a file. */
-    case SYS_TELL:                  
-
-      break;
-    
-    /* Close a file. */
-    case SYS_CLOSE:
-
-      break;
-
-    default:
-      break;
-  }
   printf ("system call!\n");
   thread_exit ();
 }
