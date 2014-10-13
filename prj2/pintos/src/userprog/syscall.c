2da4ac65741302d5ec156db8449e9a3043b40b2d
Richard Chen <rchen@twitter.com>
2014-10-13 01:09:13 -0700

    add write to syscall.c

--- prj2/pintos/src/userprog/syscall.c
+++ prj2/pintos/src/userprog/syscall.c
@@ -3,8 +3,13 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "userprog/process.h"
+#include "filesys/file.h"
+
+struct lock filesys_lock;
 
 static void syscall_handler (struct intr_frame *);
+struct file* process_get_file(int fd);  //return file by file descriptor
 
 void
 syscall_init (void) 
@@ -18,3 +23,35 @@ syscall_handler (struct intr_frame *f UNUSED)
   printf ("system call!\n");
   thread_exit ();
 }
+
+int wait(pid_t pid)
+{
+	return process_wait(pid);
+}
+
+struct file* process_get_file(int fd)
+{
+	return NULL;
+}
+int write(int fd, const void *buffer, unsigned size) 
+{
+	//Fd=1(STDOUT_FILENO) writes to the console. 
+	if(fd == STDOUT_FILENO)
+	{
+		putbuff(buffer,size);
+		return size;
+	}
+	lock_acquire(&filesys_lock);
+	// return file by file descriptor.
+	struct file *f = process_get_file(fd);
+	if(!file) {
+		lock_release(&filesys_lock);
+		return ERROR; //-1
+	}
+	//return number of bytes actually written, maybe less than size if end of file is reached.
+		int size = file_read(f, buffer, size);
+		lock_release(*filesys_lock);
+		return size;
+}
+
+		
